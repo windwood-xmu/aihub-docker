3d60ffaedfa72784e2f8e56a20ff187cc42682f3
windwood-xmu <windwood@xmu.edu.cn>
2019-11-13 19:53:31 +0800

    add jupyterhub service with dockerspawner TODO traefik does not connect to jupyterhub

---
+++ jupyterhub/jupyterhub_config.py
@@ -0,0 +1,15 @@
+import os
+
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+## Services
+c.JupyterHub.services = [
+    {
+        'name': 'cull_idle',
+        'admin': True,
+        'command': 'python /srv/jupyterhub/cull_idle_servers.py --timeout=3600'.split(),
+    },
+]
