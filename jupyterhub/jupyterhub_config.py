c3bec221e7e891d45632da6096fda74f539f7f61
windwood-xmu <windwood@xmu.edu.cn>
2019-11-13 20:18:14 +0800

    add more function to jupyterhub authentication

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,9 +1,29 @@
 import os
 
+## Generic
+c.JupyterHub.admin_access = True
+#c.Spawner.default_url = '/lab'
+
+## Authenticator
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
+c.DummyAuthenticator.password = 'xmu2019'
+
+c.Authenticator.whitelist = {
+        'a01',
+        'a02',
+        'a03',
+        }
+
+c.Authenticator.admin_users = { 'aihub' }
+
+
+## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
 
 ## Services
 c.JupyterHub.services = [
